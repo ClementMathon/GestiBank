edd2fa64896a3d9e210db98050d206ae71f7d619
jcgwebdeveloper <jc.gouleau@gmail.com>
2019-04-25 09:50:28 +0200

    mise a jour version

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavadminComponent } from './navadmin/navadmin.component';
+import { AffectationComponent } from './affectation/affectation.component';
+import { AgentProfileComponent } from './agent-profile/agent-profile.component';
 
 @NgModule({
   imports: [
@@ -8,7 +10,7 @@ import { NavadminComponent } from './navadmin/navadmin.component';
 
   ],
 
-  declarations: [NavadminComponent],
+  declarations: [NavadminComponent, AffectationComponent, AgentProfileComponent],
   exports: [
     NavadminComponent
   ]
