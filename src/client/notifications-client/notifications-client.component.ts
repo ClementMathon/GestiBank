bb0439eba00485bc3bd2a8a8f1cf81d586c8f271
nieluj1981 <julien.sejourne@gmail.com>
2019-04-25 09:44:34 +0200

    julien creation composants

---
+++ src/client/notifications-client/notifications-client.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-notifications-client',
+  templateUrl: './notifications-client.component.html',
+  styleUrls: ['./notifications-client.component.scss']
+})
+export class NotificationsClientComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/client/virement-client/virement-client.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-virement-client',
+  templateUrl: './virement-client.component.html',
+  styleUrls: ['./virement-client.component.scss']
+})
+export class VirementClientComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
