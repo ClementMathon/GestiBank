7a9c1136450d6c414a712ceb05a839b39d54eaea
Marc Ellis <mhe.mex@gmail.com>
2019-04-25 12:11:02 +0200

    créé footer navbar sidebar pour conseiller

---
+++ src/conseiller/footer-conseiller/footer-conseiller.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-conseiller',
+  templateUrl: './footer-conseiller.component.html',
+  styleUrls: ['./footer-conseiller.component.scss']
+})
+export class FooterConseillerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
