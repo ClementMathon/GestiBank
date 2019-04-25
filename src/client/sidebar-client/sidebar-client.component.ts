c9da780058fd2521e772cc496c462c4ecf43bfeb
nieluj1981 <julien.sejourne@gmail.com>
2019-04-25 09:55:35 +0200

    julien routes1

--- src/client/sidebar-client/sidebar-client.component.ts
+++ src/client/sidebar-client/sidebar-client.component.ts
@@ -1,5 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+declare const $: any;
+declare interface RouteInfo {
+    path: string;
+    title: string;
+    icon: string;
+    class: string;
+}
+export const ROUTES: RouteInfo[] = [
+    { path: '/', title: 'admin',  icon: 'dashboard', class: '' },
+    { path: '/user-profile', title: 'User Profile',  icon:'person', class: '' },
+    { path: '/table-list', title: 'Table List',  icon:'content_paste', class: '' },
+    { path: '/typography', title: 'Typography',  icon:'library_books', class: '' },
+    { path: '/icons', title: 'Icons',  icon:'bubble_chart', class: '' },
+    { path: '/notifications', title: 'Notifications',  icon:'notifications', class: '' },
+];
 @Component({
   selector: 'app-sidebar-client',
   templateUrl: './sidebar-client.component.html',
