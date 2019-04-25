abddadaa35ef0e912d40935b8625c6e075071a00
Marc Ellis <mhe.mex@gmail.com>
2019-04-25 10:17:42 +0200

    ajouté conseiller layout component (vide)

---
+++ src/conseiller/conseiller-layout/conseiller-layout.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ConseillerLayoutComponent } from './conseiller-layout.component';
+
+describe('ConseillerLayoutComponent', () => {
+  let component: ConseillerLayoutComponent;
+  let fixture: ComponentFixture<ConseillerLayoutComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ConseillerLayoutComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ConseillerLayoutComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/conseiller/conseiller-layout/conseiller-layout.module.ts
+++ src/conseiller/conseiller-layout/conseiller-layout.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CoseillerLayoutComponent } from './coseiller-layout/coseiller-layout.component';
+import { ConseillerLayoutComponent } from './conseiller-layout.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [CoseillerLayoutComponent, ConseillerLayoutComponent]
 })
 export class ConseillerLayoutModule { }
