06eefab8ee59073e73e7b17efcdafa329fd1225a
jcgwebdeveloper <jc.gouleau@gmail.com>
2019-04-25 12:26:59 +0200

    gros changement de route jcg

--- src/admin/agent-profile/agent-profile.component.ts
+++ src/admin/agent-profile/agent-profile.component.ts
@@ -1,10 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import { NgModule } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
 @Component({
   selector: 'app-agent-profile',
   templateUrl: './agent-profile.component.html',
   styleUrls: ['./agent-profile.component.scss']
 })
+@NgModule({
+  imports: [
+    MatFormFieldModule
+  ]})
 export class AgentProfileComponent implements OnInit {
 
   constructor() { }
---
+++ src/admin/gestiondesagents/gestiondesagents.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gestiondesagents',
+  templateUrl: './gestiondesagents.component.html',
+  styleUrls: ['./gestiondesagents.component.scss']
+})
+export class GestiondesagentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
