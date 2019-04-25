66c8aa6d16bd5738bf5c5df52d8c2a0979c0ea31
nieluj1981 <julien.sejourne@gmail.com>
2019-04-25 10:04:43 +0200

    julien routes2

--- src/client/sidebar-client/sidebar-client.component.ts
+++ src/client/sidebar-client/sidebar-client.component.ts
@@ -8,12 +8,13 @@ declare interface RouteInfo {
     class: string;
 }
 export const ROUTES: RouteInfo[] = [
-    { path: '/', title: 'admin',  icon: 'dashboard', class: '' },
-    { path: '/user-profile', title: 'User Profile',  icon:'person', class: '' },
-    { path: '/table-list', title: 'Table List',  icon:'content_paste', class: '' },
-    { path: '/typography', title: 'Typography',  icon:'library_books', class: '' },
-    { path: '/icons', title: 'Icons',  icon:'bubble_chart', class: '' },
-    { path: '/notifications', title: 'Notifications',  icon:'notifications', class: '' },
+    { path: '/', title: 'client',  icon: 'dashboard', class: '' },
+    { path: '/comptes-client', title: 'Comptes Client',  icon:'person', class: '' },
+    { path: '/virement-client', title: 'Virement Client',  icon:'person', class: '' },
+    { path: '/profil-client', title: 'Profil Client',  icon:'content_paste', class: '' },
+    { path: '/historique-client', title: 'Historique Client',  icon:'library_books', class: '' },
+    { path: '/commandes-client', title: 'Commandes Client',  icon:'bubble_chart', class: '' },
+    { path: '/notifications-client', title: 'Notifications Client',  icon:'notifications', class: '' },
 ];
 @Component({
   selector: 'app-sidebar-client',
@@ -21,10 +22,17 @@ export const ROUTES: RouteInfo[] = [
   styleUrls: ['./sidebar-client.component.scss']
 })
 export class SidebarClientComponent implements OnInit {
+  menuItems: any[];
 
   constructor() { }
 
   ngOnInit() {
+    this.menuItems = ROUTES.filter(menuItem => menuItem);
   }
-
+  isMobileMenu() {
+      if ($(window).width() > 991) {
+          return false;
+      }
+      return true;
+  };
 }
